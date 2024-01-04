536fb87b62fd39b4d4476894da9399faa3ede59b
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-05 00:22:32 +0300

    Add project files.

---
+++ ToDoListApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ToDoListApp.Data.Models;
+
+namespace ToDoListApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TaskModel> TaskModels { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ToDoListApp/Data/Models/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListApp.Data.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
